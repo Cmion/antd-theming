1b9b3211a171526d430abd383b277bac94d4f416
Cmion <akpanudosimeon@hotmail.com>
2021-06-11 16:46:26 +0100

    chore: added new way to add multiple teams

--- compact.js
+++ compact.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 try {
+  // Use clean-css-cli to minify
   const light = fs.readFileSync('./stm-light.css', 'utf8');
   const dark = fs.readFileSync('./stm-dark.css', 'utf8');
   const content = `html[data-theme='light'] {
